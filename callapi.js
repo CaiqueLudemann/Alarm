91cf4172358932ef5f245cddc47ba6609a693bcc
CaiqueLudemann <caiqueludemann@gmail.com>
2023-07-28 12:47:14 -0300

    add client-side verification for input types.

--- callapi.js
+++ callapi.js
@@ -1,19 +1,6 @@
-import {alarm} from './helperFunctions.js';
+import { displayMessage } from './helperFunctions.js';
 
-
-const nameInput = document.getElementById('name');
-const delayInput = document.getElementById('delay');
 const submit = document.getElementById('submit');
-const output = document.querySelector('h2');
 
-async function displayMessage() {
-  output.innerHTML = "loading...";
-  try {
-    const wakeup = await alarm(nameInput.value, parseInt(delayInput.value))
-    output.innerHTML = wakeup;
-  } catch (error) {
-    output.innerHTML = `${error}`
-  }
-}
 
 submit.addEventListener('click', displayMessage);
--- helperFunctions.js
+++ helperFunctions.js
@@ -1,4 +1,6 @@
-
+const nameInput = document.getElementById('name');
+const delayInput = document.getElementById('delay');
+const output = document.querySelector('h2');
 
 export function alarm(name, delay){
   return new Promise((resolve, reject)=>{
@@ -9,9 +11,18 @@ export function alarm(name, delay){
     }
     setTimeout(()=>{
       resolve(`Get yo ass up ${name}`);
-    },delay)
+    }, parseInt(delayInput.value))
   })
 };
 
+export async function displayMessage() {
+  output.innerHTML = "loading...";
+  try {
+    const wakeup = await alarm(nameInput.value, delayInput.value)
+    output.innerHTML = wakeup;
+  } catch (error) {
+    output.innerHTML = `${error}`
+  }
+}
 
 
